f8a6ed9e98b932de093bde7e3a3d0d58f1efa22c
KhadafiZubaidiDev <khadafizubaidi.dev@gmail.com>
2023-09-21 19:20:49 +0700

    Initial commit

---
+++ lib/traditional_coffee_shop_theme_provider.dart
@@ -0,0 +1,36 @@
+library traditional_coffee_shop_theme_provider;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class TraditionalCoffeeShopThemeProvider extends ChangeNotifier {
+  ThemeMode themeMode = ThemeMode.system;
+  bool get isDarkMode {
+    if (themeMode == ThemeMode.system) {
+      final brightness = SchedulerBinding.instance.window.platformBrightness;
+      return brightness == Brightness.dark;
+    } else {
+      return themeMode == ThemeMode.dark;
+    }
+  }
+
+  void toggleTheme(bool isOn) {
+    themeMode = isOn ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
+
+class CoffeeShopTheme {
+  static final darkTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.black,
+    primaryColor: Colors.black,
+    colorScheme: const ColorScheme.dark(),
+    iconTheme: const IconThemeData(color: Colors.white, opacity: 0.8),
+  );
+
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.brown[400],
+    primaryColor: Colors.brown[400],
+    colorScheme: const ColorScheme.light(),
+    iconTheme: const IconThemeData(color: Colors.white, opacity: 0.8),
+  );
+}
---
+++ test/traditional_coffee_shop_theme_provider_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:traditional_coffee_shop_theme_provider/traditional_coffee_shop_theme_provider.dart';
+
+void main() {
+
+}
